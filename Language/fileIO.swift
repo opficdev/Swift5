1bd3c7f3ad53d35d405c60939a3b9ab6838bfec7
opficdev <opficdev@gmail.com>
2024-06-25 01:11:52 +0900

    예외에 대한 출력사항 추가

--- Language/fileIO.swift
+++ Language/fileIO.swift
@@ -5,21 +5,28 @@ class fstream {
     private var buffer: Data
     private let bufferSize: Int
     private let fileURL: URL
-    private let job: String
+    private let mode: String
     
-    init?(_ fileName: String, _ job: String, bufferSize: Int = 4096) {
+    init?(_ fileName: String, _ mode: String, bufferSize: Int = 4096) {
+        if mode != "r" && mode != "w"{
+            print("Wrong mode")
+            return nil
+        }
         self.fileURL = URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent(fileName)
-        self.job = job
-         do {
-             if job == "r"{
-                 self.fileHandle = try FileHandle(forReadingFrom: self.fileURL)
-             }
-         } catch {
-             return nil
-         }
-         
-         self.buffer = Data(capacity: bufferSize)
-         self.bufferSize = bufferSize
+        
+        do {
+            if mode == "r"{
+                self.fileHandle = try FileHandle(forReadingFrom: self.fileURL)
+            }
+            else{
+                self.fileHandle = FileHandle() //dummy
+            }
+        } catch {
+            return nil
+        }
+        self.mode = mode
+        self.buffer = Data(capacity: bufferSize)
+        self.bufferSize = bufferSize
      }
     deinit {
         if fileHandle != nil{
@@ -27,12 +34,14 @@ class fstream {
         }
     }
     
+    
     func readLine() -> String? {
-        var line = Data()
-        var byte: UInt8 = 0
-        if self.job != "r"{
+        guard self.mode == "r" else {
+            print("This function can use read mode only.")
             return nil
         }
+        var line = Data()
+        var byte: UInt8 = 0
         
         while true {
             if buffer.isEmpty {
@@ -41,22 +50,26 @@ class fstream {
                     return line.isEmpty ? nil : String(data: line, encoding: .utf8)!
                 }
             }
-            
+
             byte = buffer.removeFirst()
             
-            if byte == 10 { // 10은 ASCII 코드로 줄바꿈(\n)을 의미합니다.
+            if byte == 10{ // 10: \n
                 return String(data: line, encoding: .utf8)!
             }
             
-            line.append(byte)
+            if byte != 13{ //13: \r
+                line.append(byte)
+            }
         }
     }
     
-    func read() -> [[String]]{
-        var lines: [[String]] = []
-        if self.job != "r"{
-            return lines
+    func read() -> [[String]]?{
+        guard self.mode == "r" else {
+            print("This function can use read mode only.")
+            return nil
         }
+        
+        var lines: [[String]] = []
         do {
             let fileHandle = try FileHandle(forReadingFrom: fileURL)
             
@@ -66,7 +79,6 @@ class fstream {
             if let content = String(data: data, encoding: .utf8) {
                 fileContent = content
             }
-            fileHandle.closeFile()
             
             let filter = fileContent.contains("\r\n") ? "\r\n" : "\n"
             
@@ -79,15 +91,17 @@ class fstream {
         return lines
     }
     
-    func write(_ str: String){
-        if self.job == "w"{
-            do {
-                try str.write(to: fileURL, atomically: true, encoding: .utf8)
-            } catch {
-                
-            }
+    func write(_ data: Any){
+        guard self.mode == "w" else {
+            print("This function can use write mode only.")
+            return
+        }
+       
+        do {
+            let data = String(describing: data)
+            try data.write(to: fileURL, atomically: true, encoding: .utf8)
+        } catch {
+            print("Something wrong write data!!")
         }
     }
-    
-    
 }
---
+++ ProblemSolve/그릇/dish.swift
@@ -0,0 +1,8 @@
+//
+//  dish.swift
+//  Swift5
+//
+//  Created by 최윤진 on 2024-06-24.
+//
+
+import Foundation
