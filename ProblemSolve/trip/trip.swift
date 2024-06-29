47285d057e82fa579ef0e79f75c88daa9d6ac150
opficdev <opficdev@gmail.com>
2024-06-30 01:16:00 +0900

    trip 문제 구현

--- ProblemSolve/trip/trip.swift
+++ ProblemSolve/trip/trip.swift
@@ -1,16 +1,42 @@
-var result = ""
+import Foundation
 
-while true{
-    let s = Int(readLine()!)!
-    if s == 0 { break }
-    var money:[Double] = []
-    for _ in 0..<s{
-        money.append(Double(readLine()!)!)
+@main
+struct Main{
+    static func main(){
+        var res = ""
+        let fin = FileReader("trip.inp")!
+        let fout = FileWriter("trip.out")
+        
+        while true{
+            let s = Int(fin.readLine()!)!
+            if s == 0 { break }
+            
+            var money:[Double] = []
+            
+            var sum = 0.0
+            for _ in 0..<s{
+                money.append(Double(fin.readLine()!)!)
+                sum += money.last!
+            }
+            
+            let avg = sum / Double(s)
+            
+            var posSum = 0.0, negSum = 0.0
+            
+            for i in money{
+                let diff = Double(Int((i - avg) * 100)) / 100.0
+                if diff >= 0 {
+                    posSum += diff
+                }
+                else {
+                    negSum -= diff
+                }
+            }
+            
+            res += String(format: "$%.2f", max(posSum, negSum)) + "\n"
+            
+        }
+        fout.write(res)
     }
-    let avg = Double(Int(money.reduce(0.0, +) / Double(s)))
-    
-    result += "\(money.map{ $0 > avg ? 0 : avg - $0 }.reduce(0, +))\n"
 }
 
-
-print(result)
