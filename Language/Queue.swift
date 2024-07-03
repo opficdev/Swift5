03d2aebfc58bcb941ada74596b1ccf1cc2d65397
opficdev <opficdev@gmail.com>
2024-07-03 16:02:24 +0900

    C++의 std::queue 구현

--- Language/Queue.swift
+++ Language/Queue.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+
+class Queue<T>{
+    class node<t>{
+        var data: t? = nil, front: node? = nil, back: node? = nil //front: head쪽으로 가는 다음 노드 / back: tail쪽으로 가는 다음 노드
+        init(){}
+        init(_ data: t){
+            self.data = data
+        }
+    }
+    //output node
+    private var head: node<T>? = nil
+    //input node
+    private var tail: node<T>? = nil
+    
+    var count = 0
+    
+    ///Only declaration.
+    init(){}
+    ///Declaration with input datas.
+    init(_ datas: [T]){
+        for data in datas{
+            append(data)
+        }
+    }
+    
+    
+    var isEmpty: Bool {return self.head == nil && self.tail == nil}
+    
+    ///Get head
+    var first: T?{
+        return self.head?.data
+    }
+    ///get tail
+    var last: T?{
+        return self.tail?.data
+    }
+        
+    ///Input data into queue.
+    func append(_ data: T){
+        self.count += 1
+        let newNode = node(data)
+        if self.isEmpty{
+            self.head = newNode
+            self.tail = self.head
+        }
+        else{
+            newNode.front = self.tail
+            self.tail!.back = newNode
+            self.tail = newNode
+        }
+    }
+    
+    ///Get data from queue.
+    func remove(){
+        if !self.isEmpty{
+            self.head = self.head!.back
+            self.head?.front = nil
+            if self.head == nil{
+                self.tail = nil
+            }
+            self.count -= 1
+        }
+    }
+    
+    func pop() -> T?{ ///Get data from queue and remove last node.
+        let data = first
+        remove()
+        return data
+    }
+}
