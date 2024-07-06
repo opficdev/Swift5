f35cb03e07fcc8d84ad8239d464f3fe8d8959046
opficdev <opficdev@gmail.com>
2024-07-07 01:01:27 +0900

    Adding Cost 문제 구현

--- ProblemSolve/Adding Cost/add.swift
+++ ProblemSolve/Adding Cost/add.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+@main
+struct Main{
+    static func main(){
+        let fin = FileReader("add.inp")!
+        let fout = FileWriter("add.out")
+        var res = ""
+    
+        while true{
+            let N = Int(fin.readLine()!)!
+            if N == 0{
+                break
+            }
+            var pq = PriorityQueue(fin.readLine()!.split(separator: " ").map({UInt64($0)!}), by:>)
+            var total: UInt64 = 0
+            while pq.count > 1{
+                let n1 = pq.pop()!, n2 = pq.pop()!
+                total += n1 + n2
+                pq.append(n1 + n2)
+            }
+            res += "\(total)\n"
+        }
+        fout.write(res)
+    }
+}
+
